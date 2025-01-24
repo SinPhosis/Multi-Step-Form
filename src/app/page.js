59db9f27b084c9b1827cee29b251ff026677f8ff
SinPhosis <sionclasher@gmail.com>
2025-01-24 21:45:30 +0800

    .

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,13 @@ import { StepTwo } from "@/component/StepTwo";
 import { StepThree } from "@/component/StepThree";
 export default function Home() {
   const [step, setStep] = useState(1);
+  const [localStorage, setLocalStorageData] = useState(null);
+   useEffect(() => {
+      if (typeof window !== 'undefined' && window.localStorage) {
+        const data = localStorage.getItem('myData');
+        setLocalStorageData(data);
+      }
+    }, []);
   return (
     <>
       {step === 1 && <StepOne setStep={setStep} />}
